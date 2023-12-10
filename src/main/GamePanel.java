66542bceae71c3151aad1be89e6955016a10ad3b
Thái Quang Tính <tinhthai2000@gmail.com>
2023-12-10 20:10:03 +0700

    Add files via upload

---
+++ src/main/GamePanel.java
@@ -0,0 +1,12 @@
+package main;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+    final int originalTileSize = 16; //16x16 size
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; //48x48 tile
+    
+    
+}
---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Treasure hunting");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
