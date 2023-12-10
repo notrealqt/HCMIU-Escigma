2cbef690d019e24ce9b9b9a594e3706cc7b61331
Thái Quang Tính <tinhthai2000@gmail.com>
2023-12-11 05:31:18 +0700

    Update

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -16,19 +16,37 @@ public class GamePanel extends JPanel implements Runnable {
     
     Thread gameThread;
 
-
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
 
     }
+
     public void startGameThread () {
         gameThread = new Thread(this);
         gameThread.start();
     }
+
     @Override
     public void run() {
+        while (gameThread != null) {
+            //1 Update position
+            update();
+            //2 draw the screen with updated information
+            repaint();
+        }
+    }
+    public void update() {
 
+    }
+    //draw things 
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g); //required whenever creating this method.
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white); // set color to use for drawing objects
+        g2.fillRect(100,100,tileSize,tileSize);
+        g2.dispose();
     }
 }
