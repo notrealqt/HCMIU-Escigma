90304ada0bf688025944a3386fd1fedfa84fa45f
Thái Quang Tính <tinhthai2000@gmail.com>
2023-12-11 14:42:27 +0700

    Create KeyHandle.java

---
+++ src/main/KeyHandle.java
@@ -0,0 +1,46 @@
+package main;
+
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+
+public class KeyHandle implements KeyListener {
+
+    public boolean upPressed, downPressed, leftPressed,rightPressed;
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+        if (code == KeyEvent.VK_S) {
+            downPressed = true;
+        }
+        if (code == KeyEvent.VK_A) {
+            leftPressed = true;
+        }
+        if (code == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
+    }
+
+    @Override
+    public void keyReleased (KeyEvent e) {
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_W) {
+            upPressed = false;
+        }
+        if (code == KeyEvent.VK_S) {
+            downPressed = false;
+        }
+        if (code == KeyEvent.VK_A) {
+            leftPressed = false;
+        }
+        if (code == KeyEvent.VK_D) {
+            rightPressed = false;
+        }
+    }
+}
