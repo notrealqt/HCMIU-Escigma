ea6be2c1e911c921fa7f14a1612a4d44ddb49329
Thái Quang Tính <tinhthai2000@gmail.com>
2023-12-11 14:41:27 +0700

    Update GamePanel.java

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,6 +3,8 @@ package main;
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16; //16x16 size
@@ -14,20 +16,26 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; //768 pixels
     final int screenHeight = tileSize * maxScreenRow; //576 pixels
     
+    KeyHandle KeyH = new KeyHandle();
     Thread gameThread;
+
+    //set player's dafault position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
-
+        this.addKeyListener(KeyH);
+        this.setFocusable(true);
     }
-
     public void startGameThread () {
         gameThread = new Thread(this);
         gameThread.start();
     }
-
     @Override
     public void run() {
         while (gameThread != null) {
@@ -38,7 +46,18 @@ public class GamePanel extends JPanel implements Runnable {
         }
     }
     public void update() {
-
+        if (KeyH.upPressed == true) {
+            playerY -= playerSpeed;
+        }
+        else if (KeyH.downPressed == true) {
+            playerY += playerSpeed;
+        }
+        else if (KeyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        }
+        else if ( KeyH.rightPressed == true) {
+            playerX += playerSpeed;
+        }
     }
     //draw things 
     public void paintComponent(Graphics g) {
@@ -46,7 +65,7 @@ public class GamePanel extends JPanel implements Runnable {
         Graphics2D g2 = (Graphics2D)g;
 
         g2.setColor(Color.white); // set color to use for drawing objects
-        g2.fillRect(100,100,tileSize,tileSize);
+        g2.fillRect(playerX,playerY,tileSize,tileSize);
         g2.dispose();
     }
 }
