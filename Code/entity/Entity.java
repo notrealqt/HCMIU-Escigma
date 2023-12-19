cb73b7161eaa2e704cef7a3f83af842076cdf3b8
Thái Quang Tính <tinhthai2000@gmail.com>
2023-12-19 20:10:58 +0700

    update Player.java + add image files

--- Code/entity/Entity.java
+++ Code/entity/Entity.java
@@ -6,6 +6,7 @@ public class Entity {
     public int x,y; // variables for all characters in the game
     public int speed;
 
-    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public BufferedImage up1, up2, up3, up0, up4, up5, up6, up7,up8,up9;
+    public BufferedImage down1, down2, left1, left2, right1, right2;
     public String direction;
 }
--- Code/entity/Player.java
+++ Code/entity/Player.java
@@ -1,7 +1,6 @@
 package entity;
-
-import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -19,20 +18,44 @@ public class Player extends Entity {
         this.keyH = keyH;
         
         setDefaultValue();
-
+        getPlayerImage();
     }
 
     public void setDefaultValue() {
         x= 100;
         y = 100;
         speed = 4;
+        direction = "down";
     }
 
     public void getPlayerImage() {
         try {
-            up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+            //up
+            up0 = ImageIO.read(getClass().getResourceAsStream("/res/player/1_player_move_font_0.png"));
+            up1 = ImageIO.read(getClass().getResourceAsStream("/res/player/1_player_move_font_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/res/player/1_player_move_font_2.png"));
+            up3 = ImageIO.read(getClass().getResourceAsStream("/res/player/1_player_move_font_3.png"));
+            up4 = ImageIO.read(getClass().getResourceAsStream("/res/player/1_player_move_font_4.png"));
+            up5 = ImageIO.read(getClass().getResourceAsStream("/res/player/1_player_move_font_5.png"));
+            up6 = ImageIO.read(getClass().getResourceAsStream("/res/player/1_player_move_font_6.png"));
+            up7 = ImageIO.read(getClass().getResourceAsStream("/res/player/1_player_move_font_7.png"));
+            up8 = ImageIO.read(getClass().getResourceAsStream("/res/player/1_player_move_font_8.png"));
+            up9 = ImageIO.read(getClass().getResourceAsStream("/res/player/1_player_move_font_9.png"));
+
+
+
+
+
+
+
+            down1 = ImageIO.read(getClass().getResourceAsStream("/res/player/1_player_move_back_0.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/res/player/1_player_move_back_1.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/res/player/1_player_move_left_0.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/res/player/1_player_move_left_1.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/res/player/1_player_move_right_0.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/res/player/1_player_move_right_1.png"));
         } catch (IOException e) {
-            // TODO: handle exception
+            e.printStackTrace();
         }
     }
 
@@ -40,21 +63,25 @@ public class Player extends Entity {
         //System.out.println("Update method called");
 
         if (keyH.upPressed == true) {
+            direction = "up";
             y -= speed;
             //System.out.println("Up pressed: " + playerX + ", " + playerY);
 
         }
         else if (keyH.downPressed == true) {
+            direction = "down";
             y += speed;
             //System.out.println("Down pressed: " + playerX + ", " + playerY);
 
         }
         else if (keyH.leftPressed == true) {
+            direction = "left";
             x -= speed;
             //System.out.println("Left pressed: " + playerX + ", " + playerY);
 
         }
         else if ( keyH.rightPressed == true) {
+            direction = "right";
             x+= speed;
             //System.out.println("Right pressed: " + playerX + ", " + playerY);
 
@@ -62,7 +89,24 @@ public class Player extends Entity {
     }
 
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.white); // set color to use for drawing objects
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        //g2.setColor(Color.white); // set color to use for drawing objects
+        //g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+
+        BufferedImage image = null;
+        switch (direction) {
+            case "up":
+                image = up1;
+                break;
+            case "down":
+                image = down1;
+                break;
+            case "left":
+                image = left1;
+                break;
+            case "right":
+                image = right1;
+                break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }
